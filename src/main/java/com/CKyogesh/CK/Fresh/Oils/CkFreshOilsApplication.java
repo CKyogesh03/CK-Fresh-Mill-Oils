6350c4fc8f601593cd54d8cdddaa073d3cd8dc63
CKyogesh03 <115444713+CKyogesh03@users.noreply.github.com>
2022-11-01 20:35:03 +0530

    initial commit

---
+++ src/main/java/com/CKyogesh/CK/Fresh/Oils/CkFreshOilsApplication.java
@@ -0,0 +1,13 @@
+package com.CKyogesh.CK.Fresh.Oils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkFreshOilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkFreshOilsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CKyogesh/CK/Fresh/Oils/CkFreshOilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CKyogesh.CK.Fresh.Oils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkFreshOilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
